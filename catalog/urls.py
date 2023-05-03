34b4564480d6baf6e7458a1ee259dae33140b8a6
Paul Leelaviwatana <apaulture@gmail.com>
2023-05-02 17:49:50 -0700

    Save point: 5-Configuring where to find the templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#     path('catalog/books/', views.BookListView.as_view(), name='book-list'),
-#     path('catalog/books/<id>/', views.BookDetailView.as_view, name='book-detail'),
-#     path('catalog/authors/', views.AuthorListView.as_view(), name='author-list'),
-#     path('catalog/authors/<id>/', views.AuthorDetailView.as_view, name='author-detail')
+    path('', views.index, name='index'),
+    # path('books/', views.BookListView.as_view(), name='book-list'),
+    # path('books/<id>/', views.BookDetailView.as_view, name='book-detail'),
+    # path('authors/', views.AuthorListView.as_view(), name='author-list'),
+    # path('authors/<id>/', views.AuthorDetailView.as_view, name='author-detail')
 ]
